1345eb5322062caa8d9062b79f757c84a68f2c87
Qilin Lou <qilin.lou@thoughtworks.com>
2020-01-16 17:36:47 +0800

    First commit - match rule 1&2

---
+++ src/main/java/com/training/Main.java
@@ -0,0 +1,43 @@
+package com.training;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            String result = fizzBuzz(i + 1);
+            System.out.println(result);
+        }
+    }
+
+    static String fizzBuzz(int input) {
+
+        if (input % (3 * 5 * 7) == 0) {
+            return "FizzBuzzWhizz";
+        }
+
+        if (input % (5 * 7) == 0) {
+            return "BuzzWhizz";
+        }
+
+        if (input % (3 * 7) == 0) {
+            return "FizzWhizz";
+        }
+
+        if (input % (3 * 5) == 0) {
+            return "FizzBuzz";
+        }
+
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (input % 5 == 0) {
+            return "Buzz";
+        }
+
+
+        if (input % 7 == 0) {
+            return "Whizz";
+        }
+        return String.valueOf(input);
+    }
+}
---
+++ src/test/java/com/training/MainTest.java
@@ -0,0 +1,104 @@
+package com.training;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void fizzBuzz_is_3() {
+        //given
+        int input = 3;
+        String except = "Fizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_3_multiples() {
+        //given
+        int input = 6;
+        String except = "Fizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+
+        //given
+        int input2 = 9;
+        //when
+        String actual2 = Main.fizzBuzz(input2);
+        //then
+        assertEquals(except, actual2);
+    }
+
+    @Test
+    public void fizzBuzz_is_5_multiples() {
+        //given
+        int input = 5;
+        String except = "Buzz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_7_multiples() {
+        //given
+        int input = 7;
+        String except = "Whizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_3_5_multiples() {
+        //given
+        int input = 3 * 5;
+        String except = "FizzBuzz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_3_7_multiples() {
+        //given
+        int input = 3 * 7;
+        String except = "FizzWhizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_5_7_multiples() {
+        //given
+        int input = 5 * 7;
+        String except = "BuzzWhizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+    @Test
+    public void fizzBuzz_is_3_5_7_multiples() {
+        //given
+        int input = 3 * 5 * 7;
+        String except = "FizzBuzzWhizz";
+        //when
+        String actual = Main.fizzBuzz(input);
+        //then
+        assertEquals(except, actual);
+    }
+
+}
