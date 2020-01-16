7876159f59633c3e3ef682cc55dc2a619dcc97a2
Qilin Lou <qilin.lou@thoughtworks.com>
2020-01-17 00:25:13 +0800

    refactor by consider org

--- src/main/java/com/training/Main.java
+++ src/main/java/com/training/Main.java
@@ -1,48 +1,62 @@
 package com.training;
 
+import javafx.beans.binding.When;
+
 public class Main {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
+
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 120; i++) {
             String result = fizzBuzz(i + 1);
             System.out.println(result);
         }
     }
 
     static String fizzBuzz(int input) {
+        boolean isDivisible3 = input % 3 == 0;
+        boolean isDivisible5 = input % 5 == 0;
+        boolean isDivisible7 = input % 7 == 0;
 
         String inputStr = String.valueOf(input);
+        boolean isContain3 = inputStr.contains("3");
+        boolean isContain5 = inputStr.contains("5");
+        boolean isContain7 = inputStr.contains("7");
 
-        if (inputStr.contains("3")) {
-            return "Fizz";
+        if (isContain3) {
+            return FIZZ;
         }
 
-        if (input % (3 * 5 * 7) == 0) {
-            return "FizzBuzzWhizz";
+        if (isDivisible3 && isDivisible5 && isDivisible7) {
+            return FIZZ + BUZZ + WHIZZ;
         }
 
-        if (input % (5 * 7) == 0) {
-            return "BuzzWhizz";
+        if (isDivisible5 && isDivisible7) {
+            return BUZZ + WHIZZ;
         }
 
-        if (input % (3 * 7) == 0) {
-            return "FizzWhizz";
+        if (isDivisible3 && isDivisible7) {
+            return FIZZ + WHIZZ;
         }
 
-        if (input % (3 * 5) == 0) {
-            return "FizzBuzz";
+        if (isDivisible3 && isDivisible5) {
+            return FIZZ + BUZZ;
         }
 
-        if (input % 3 == 0) {
-            return "Fizz";
+        if (isDivisible3) {
+            return FIZZ;
         }
 
-        if (input % 5 == 0) {
-            return "Buzz";
+        if (isDivisible5) {
+            return BUZZ;
         }
 
 
-        if (input % 7 == 0) {
-            return "Whizz";
+        if (isDivisible7) {
+            return WHIZZ;
         }
         return String.valueOf(input);
     }
